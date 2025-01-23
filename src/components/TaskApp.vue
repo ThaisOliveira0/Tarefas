f6cde32a5618786016f425b5554bf17162438c83
Thais Gomes de oliveira e Silva <t.silva@viainvest.local>
2025-01-23 09:01:48 -0300

    first commit

---
+++ src/components/TaskApp.vue
@@ -0,0 +1,79 @@
+<template>
+  <div @click="$emit('taskStateChanged', task)"
+    class="task-app" :class="stateClass">
+    <span @click.stop="$emit('taskDeleted',task)" class="close">x</span>
+    <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: { type: Object, required: true },
+  },
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.task-app {
+  position: relative;
+  box-sizing: border-box;
+  height: 100px;
+  width: 200px;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  font-weight: 400;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pending {
+    border-left: 12px solid #b73229;
+    color: #ddd;
+    background-color: #f44336;
+    font-size: 1.75rem;
+    text-align: center;
+}
+
+.done {
+    color: #ddd;
+    border-left: 12px solid #0a8f08;
+    background-color: #4caf50;
+    text-decoration: line-through;
+     font-size: 1.75rem;
+    text-align: center;
+}
+
+.pending .close {
+  background-color: #B73229;
+}
+
+.done .close {
+  background-color:#0a8f08 ;
+}
+
+.close {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  font-size:0.9rem ;
+  font-weight: 600;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="task-grid">
+    <template v-if="tasks.length">
+      <TaskApp class="task"  v-for="(task, i) in tasks" :key="task.name" 
+      @taskDeleted="$emit('taskDeleted', i)"
+      @taskStateChanged ="$emit('taskStateChanged',i)"
+      :task="task" />
+    </template>
+    <p v-else class="no-task"  >Sua vida está em dia, por enquanto...</p>
+  </div>
+</template>
+  
+<script>
+import TaskApp from "./TaskApp.vue";
+
+export default {
+  components: { TaskApp },
+  props: {
+    tasks: { type: Array, required: true },
+  },
+  mounted() {
+    console.log(this.tasks); // Verifique se as tarefas estão chegando corretamente
+  },
+};
+</script>
+
+<style>
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  }
+
+.task-grid .task {
+  margin: 15px;
+
+}
+
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
+</style>
